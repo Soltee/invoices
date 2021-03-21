4833422644a115546cca400e695e6ca342c55082
Soltee <soltee.13@gmail.com>
2021-03-21 15:49:24 +0545

    updated npm and composer packages.

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,8 +10,11 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
+mix.override((config) => {
+    delete config.watchOptions;
+});
 
-mix.js('resources/js/app.js', 'public/js')
+mix.js('resources/js/app.js', 'public/js').vue()
     .postCss('resources/css/app.css', 'public/css', [
         require('postcss-import'),
         require('tailwindcss'),
